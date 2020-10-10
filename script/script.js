435cb531e49dffb71984f8e57c6a6e4d45e44482
Jatematik <freerun_dimon@mail.ru>
2020-10-10 21:12:45 +0600

    Присвоил значения переменным

--- script/script.js
+++ script/script.js
@@ -1,9 +1,23 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 25000;
+let income = 'Фриланс';
+let addExpenses = 'Квартира, продукты, кошка';
+let deposit = true;
+let mission = 100000;
+let period = 6;
 
-alert('Начинаю курс по JS');
+// alert('Начинаю курс по JS');
 console.log('Привет!');
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+
+addExpenses.toLowerCase();
+console.log(addExpenses.split(', '));
+
+let budgetDay = money/30;
+console.log(budgetDay);
