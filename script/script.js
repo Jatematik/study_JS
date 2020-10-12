b12bc8df8da166d86844c132009984a35db1668e
Jatematik <freerun_dimon@mail.ru>
2020-10-13 01:21:56 +0600

    feat: created new variables,added methods for 'money, addExpenses, deposit', wrote a construction of conditions for 'budgetDay'

--- script/script.js
+++ script/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 let money = 25000;
 let income = 'Фриланс';
 let addExpenses = 'Квартира, продукты, кошка';
@@ -41,3 +43,22 @@ num = opNumbers(num);
 num = num ** 3;
 console.log(num);
 console.log(String(num).slice(0, 2));
+
+// УРОК 3
+
+money = Number(prompt('Ваш месячный доход?'));
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+deposit = confirm('Есть ли у вас депозит в банке?');
+let expenses1 = prompt('Введите обязательную статью расходов');
+let amount1 = Number(prompt('Во сколько это обойдется?'));
+let expenses2 = prompt('Введите обязательную статью расходов');
+let amount2 = Number(prompt('Во сколько это обойдется?'));
+let budgetMonth = money - amount1 - amount2;
+console.log('Бюджет на месяц равен' + budgetMonth);
+console.log('Цель будет достигнута за: ' + Math.ceil(mission/budgetMonth) + ' месяцев');
+budgetDay = Math.floor(budgetMonth/30);
+console.log('Бюджет на день: ' + budgetDay);
+if (budgetDay > 1200){console.log('У вас высокий уровень дохода!');}
+if (1200 > budgetDay > 600){console.log('У вас средний уровень дохода');}
+if (600 > budgetDay > 0){console.log('К сожалению у вас уровень дохода ниже среднего');}
+if (budgetDay < 0){console.log('Что-то пошло не так');}
